567b2a9dca55f4725a967603efe7c52e4f98a873
YAHIAQOUS <yahiaqous@gmail.com>
2023-02-02 15:45:28 +0300

    Created urls file for the Items App

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import os
-from flask import request
 
 # App Initialization
 from . import create_app # from __init__ file
 app = create_app(os.getenv("CONFIG_MODE")) 
-
-from .src.items.controllers import list_all_items_controller, create_item_controller, retrieve_item_controller, update_item_controller, delete_item_controller
 
 # ----------------------------------------------- #
 
@@ -16,21 +13,8 @@ def hello():
 
 # Applications Routes
 from .src.accounts import urls
+from .src.items import urls
 
-# Items:
-@app.route("/items", methods=['GET', 'POST'])
-def list_create_items():
-    if request.method == 'GET': return list_all_items_controller()
-    if request.method == 'POST': return create_item_controller()
-    else: return 'Method is Not Allowed'
-    
-@app.route("/items/<item_id>", methods=['GET', 'PUT', 'DELETE'])
-def retrieve_update_destroy_items(item_id):
-    if request.method == 'GET': return retrieve_item_controller(item_id)
-    if request.method == 'PUT': return update_item_controller(item_id)
-    if request.method == 'DELETE': return delete_item_controller(item_id)
-    else: return 'Method is Not Allowed'
-    
 # ----------------------------------------------- #
 
 if __name__ == "__main__":
---
+++ src/items/urls.py
@@ -0,0 +1,17 @@
+from flask import request
+
+from ...app import app
+from .controllers import list_all_items_controller, create_item_controller, retrieve_item_controller, update_item_controller, delete_item_controller
+
+@app.route("/items", methods=['GET', 'POST'])
+def list_create_items():
+    if request.method == 'GET': return list_all_items_controller()
+    if request.method == 'POST': return create_item_controller()
+    else: return 'Method is Not Allowed'
+    
+@app.route("/items/<item_id>", methods=['GET', 'PUT', 'DELETE'])
+def retrieve_update_destroy_items(item_id):
+    if request.method == 'GET': return retrieve_item_controller(item_id)
+    if request.method == 'PUT': return update_item_controller(item_id)
+    if request.method == 'DELETE': return delete_item_controller(item_id)
+    else: return 'Method is Not Allowed'
